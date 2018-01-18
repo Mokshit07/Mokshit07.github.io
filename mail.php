552c57e8feb4c7912a1af5abfe9f3eba158bd068
Mokshit <mokshitmehta13@gmail.com>
2018-01-18 13:45:54 +0530

    my new port updated

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+$to = "mokshitmehta13@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: admin@silverwingtechnologies.com";
+
+
+mail($to,$subject,$txt,$headers);
+?>
