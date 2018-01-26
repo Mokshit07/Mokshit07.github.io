e7445b27e24e8eef348806467d261809252c2e80
mokshit <mokshitmehta13@gmail.com>
2018-01-26 14:55:07 +0530

    portfolio

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "mokshitmehta13@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: admin@silverwingtechnologies.com";
-
-
-mail($to,$subject,$txt,$headers);
-?>
